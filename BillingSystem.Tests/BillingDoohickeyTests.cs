b7bb774bdaae774219403f594cf0cbfebf6c10c5
brollins90 <brollins90@gmail.com>
2015-11-06 19:02:59 -0700

    first test setup

--- BillingSystem.Tests/BillingDoohickeyTests.cs
+++ BillingSystem.Tests/BillingDoohickeyTests.cs
@@ -1,5 +1,6 @@
 ﻿namespace BillingSystem.Tests
 {
+    using Moq;
     using System;
     using System.Collections.Generic;
     using Xunit;
@@ -9,6 +10,14 @@
         [Fact]
         public void Monkey()
         {
+            // source of customers
+
+            // service of charging customers
+            ICustomerRepository repo = new Mock<ICustomerRepository>();
+            ICreditCardCharger charger = new Mock<ICreditCardCharger>();
+            BillingDoohickey thing = new BillingDoohickey(repo, charger);
+
+            thing.ProcessMonth(2011, 8);
 
         }
         // Monthly Billing
