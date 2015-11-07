a0db0864ab2d3c7dce6f6711852f3edf4c390753
brollins90 <brollins90@gmail.com>
2015-11-06 19:00:48 -0700

    first ish test

--- BillingSystem.Tests/BillingDoohickeyTests.cs
+++ BillingSystem.Tests/BillingDoohickeyTests.cs
@@ -2,8 +2,25 @@
 {
     using System;
     using System.Collections.Generic;
+    using Xunit;
 
     public class BillingDoohickeyTests
     {
+        [Fact]
+        public void Monkey()
+        {
+
+        }
+        // Monthly Billing
+        // Grace period for missed payments ("dunning" status)
+        // not all customers are subscribers
+        // idle customers should be automatically unsubscribed
+
+
+    }
+
+    public class Customer
+    {
+
     }
 }
